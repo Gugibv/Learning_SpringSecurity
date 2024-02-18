91655d968c86bf5c40abe144c71bb7ec580a1d1e
gugibv <gugibv@163.com>
2024-02-18 22:20:54 +0800

    add project without springsecurity

---
+++ AuthDemo/basicAuth/src/main/java/com/grey/basicAuth/config/MyWebAppConfigurer.java
@@ -0,0 +1,26 @@
+package com.grey.basicAuth.config;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import jakarta.annotation.Resource;
+
+@Component
+public class MyWebAppConfigurer implements WebMvcConfigurer {
+
+    @Resource
+    private AuthInterceptor authInterceptor;
+    //配置权限拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/**");
+    }
+    //简单配置启动页面
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry)
+    {
+        registry.addViewController("/").setViewName("redirect:/index.html");
+    }
+}
