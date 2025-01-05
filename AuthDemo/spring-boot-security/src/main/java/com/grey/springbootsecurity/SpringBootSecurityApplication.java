ea301ccfe0bcc2df2779dc48ef26ab65b6f9c098
gugibv <gugibv@163.com>
2025-01-05 17:35:04 +0800

    输出 spring security 相关的bean

--- AuthDemo/spring-boot-security/src/main/java/com/grey/springbootsecurity/SpringBootSecurityApplication.java
+++ AuthDemo/spring-boot-security/src/main/java/com/grey/springbootsecurity/SpringBootSecurityApplication.java
@@ -2,13 +2,21 @@ package com.grey.springbootsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @SpringBootApplication
 @EnableWebSecurity
 public class SpringBootSecurityApplication {
     public static void main(String[] args)
     {
-        SpringApplication.run(SpringBootSecurityApplication.class, args);
+
+       ConfigurableApplicationContext context=  SpringApplication.run(SpringBootSecurityApplication.class, args);
+
+       // security 中的所有过滤器
+        DefaultSecurityFilterChain bean = context.getBean(DefaultSecurityFilterChain.class);
+
+        System.out.println(bean);
     }
 }
