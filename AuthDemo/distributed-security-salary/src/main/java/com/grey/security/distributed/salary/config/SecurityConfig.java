06c64857c2b03829fd247e3f5879a310ebef14e9
grey <greyjing.zhou@ncsi.com.cn>
2025-04-15 16:36:53 +0800

    add oauth2 v2

---
+++ AuthDemo/distributed-security-salary/src/main/java/com/grey/security/distributed/salary/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.grey.security.distributed.salary.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/salary/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer(resourceServer -> resourceServer
+                        .jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
+}
