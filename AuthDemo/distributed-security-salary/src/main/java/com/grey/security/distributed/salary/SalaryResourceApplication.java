1bab5b4d19a2eb838b2b533eece1c932f7e873e8
grey <greyjing.zhou@ncsi.com.cn>
2025-04-15 16:25:23 +0800

    add oauth2 v1

---
+++ AuthDemo/distributed-security-salary/src/main/java/com/grey/security/distributed/salary/SalaryResourceApplication.java
@@ -0,0 +1,11 @@
+package com.grey.security.distributed.salary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryResourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryResourceApplication.class,args);
+    }
+}
---
+++ AuthDemo/distributed-security-uaa/src/main/java/com/grey/security/distributed/uaa/UaaServerApplication.java
@@ -0,0 +1,12 @@
+package com.grey.security.distributed.uaa;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UaaServerApplication.class,args);
+    }
+}
