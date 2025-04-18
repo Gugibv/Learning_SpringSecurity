54b46b11a7d7ce67b93fe50b7c3c98414b2ef5a0
grey <greyjing.zhou@ncsi.com.cn>
2025-04-18 17:53:58 +0800

    登录成功后，跳转到主页面

---
+++ security-demo-app/src/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h2>🏠 欢迎来到主页</h2>
+      <p>您已成功登录，现在可以访问受保护资源了。</p>
+    </div>
+  );
+}
+
+export default HomePage;
