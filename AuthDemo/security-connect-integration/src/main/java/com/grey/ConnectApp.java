702dd02cf307c7829d6417c209b359d46cbff6d9
grey <greyjing.zhou@ncsi.com.cn>
2025-04-21 13:00:06 +0800

    init security connect model

---
+++ AuthDemo/security-connect-integration/src/main/java/com/grey/ConnectApp.java
@@ -0,0 +1,12 @@
+package com.grey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ConnectApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectApp.class,args);
+    }
+}
